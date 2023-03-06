a280b6ba3a8c645251520a1c113ce2188ceceaa6
mappple <mappple@qq.com>
2023-03-06 23:59:48 +0800

    坚持第一天

---
+++ leetcode/No_1653_使字符串平衡的最少删除数.java
@@ -0,0 +1,21 @@
+public class No_1653_使字符串平衡的最少删除数 {
+    public int minimumDeletions(String s) {
+        int leftb = 0, righta = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'a') {
+                righta++;
+            }
+        }
+        int res = righta;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'a') {
+                righta--;
+            } else {
+                leftb++;
+            }
+            res = Math.min(res, leftb + righta);
+        }
+        return res;
+    }
+}
